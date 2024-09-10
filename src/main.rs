17382288c28df2b4d8519c10772c5195104fbf49
Naoaki Umezu <n.umezu1014@gmail.com>
2024-09-10 22:46:56 +0900

    NEXT => 3.5

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+mod handlers;
+mod repositories;
+
+use crate::repositories::{TodoRepository, TodoRepositoryForMemory};
+use axum::{
+    extract::Extension,
+    routing::{get, post},
+    Router,
+};
+use handlers::create_todo;
+use std::net::SocketAddr;
+use std::{env, sync::Arc};
+
+#[tokio::main]
+async fn main() {
+    // loggingの初期化
+    let leg_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", leg_level);
+    tracing_subscriber::fmt::init();
+
+    // サーバ立ち上げ
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
+    let addr = SocketAddr::from(([192, 168, 11, 9], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn create_app<T: TodoRepository>(repository: T) -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/todos", post(create_todo::<T>))
+        .layer(Extension(Arc::new(repository)))
+}
+
+async fn root() -> &'static str {
+    "Hello! axum!!"
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{body::Body, http::Request};
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let repository = TodoRepositoryForMemory::new();
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello! axum!!");
+    }
+}
